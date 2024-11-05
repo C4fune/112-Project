3f3a0af5e2cfc571fb545aa2964730434b72890c
Alctoria <junsung2@andrew.cmu.edu>
2024-11-05 15:21:06 -0500

    11/5/24 Update 1
    Added basic shit

--- Code/install.py
+++ Code/install.py
@@ -1,7 +1,20 @@
 from cmu_graphics import *
 
-# write code
+def onAppStart(app):
+    app.level = 0
+    app.width = 1000
+    app.height = 1000
 
-runApp()
+def onKeyPress(app, key):
+    pass
 
+def redrawAll(app):
+    drawLabel("Beggar Life", 500,100, size = 64)
+    drawLabel("Unlock your true money grabbing potential", 500, 175, size = 32)
+
+
+def main():
+    runApp()
+
+main()
 
